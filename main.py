d72f3b0bf882dbe21df3167f6e4f655c121c7a5a
Tomas-Mecius <tmecius@gmail.com>
2020-12-28 21:39:00 +0200

    adding new turtle object. Create 3 square snake.

--- main.py
+++ main.py
@@ -5,4 +5,16 @@ screen.setup(width=600, height=600)
 screen.bgcolor("black")
 screen.title("My Snake Game!")
 
+x_coordinates = [-20, 0, 20]
+all_turtles = []
+
+
+for turtle_index in range(0,3):
+    new_turtle = Turtle("square")
+    new_turtle.color("white")
+    new_turtle.goto(x=x_coordinates[turtle_index], y=0)
+    all_turtles.append(new_turtle)
+
+
+
 screen.exitonclick()
