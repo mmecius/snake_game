de22346145d2ebc4d55f47a70914b89a372e067f
Tomas-Mecius <tmecius@gmail.com>
2020-12-30 10:16:51 +0200

    add login changed. if turns right, cant turn back. and if going up, can't go down.

--- snake.py
+++ snake.py
@@ -1,7 +1,11 @@
 from turtle import Turtle
 
 STARTING_POSITIONS = [(0, -40), (0, -20), (0, 0)]
-MOVE_DISTANCE = 20
+MOVE_DISTANCE = 5
+UP = 90
+DOWN = 270
+LEFT = 180
+RIGHT = 0
 
 
 class Snake():
@@ -9,6 +13,7 @@ class Snake():
     def __init__(self):
         self.segments = []
         self.create_snake()
+        self.head = self.segments[0]
 
     def create_snake(self):
         for position in STARTING_POSITIONS:
@@ -26,16 +31,20 @@ class Snake():
         self.segments[0].forward(MOVE_DISTANCE)
 
     def up(self):
-        self.segments[0].setheading(90)
+        if self.head.heading() != DOWN:
+            self.head.setheading(UP)
 
     def down(self):
-        self.segments[0].setheading(270)
+        if self.head.heading() != UP:
+            self.head.setheading(DOWN)
 
     def left(self):
-        self.segments[0].setheading(180)
+        if self.head.heading() != RIGHT:
+            self.head.setheading(LEFT)
 
     def right(self):
-        self.segments[0].setheading(0)
+        if self.head.heading() != LEFT:
+            self.head.setheading(RIGHT)
 
 
 
